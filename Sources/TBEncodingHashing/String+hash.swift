b9e3c26e4efcce02b90ef13c0f4b6b22dcb24063
Todd Bowden <toddhokie@gmail.com>
2023-12-18 18:50:52 -0500

    update String+hash

--- Sources/TBEncodingHashing/String+hash.swift
+++ Sources/TBEncodingHashing/String+hash.swift
@@ -12,5 +12,13 @@ public extension String {
     func hash(length: Int? = nil) throws -> String {
         try self.utf8data().hash(length: length)
     }
+    
+    func hash(length: Int? = nil, default: String) -> String {
+        if let hash = try? self.utf8data().hash(length: length) {
+            return hash
+        } else {
+            return `default`
+        }
+    }
         
 }
