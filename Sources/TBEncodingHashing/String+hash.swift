5b9cb97794290be128ad516eb964f398fc4a767e
Todd Bowden <toddhokie@gmail.com>
2023-12-18 19:01:35 -0500

    add randomHash

--- Sources/TBEncodingHashing/String+hash.swift
+++ Sources/TBEncodingHashing/String+hash.swift
@@ -9,6 +9,11 @@ import Foundation
 
 public extension String {
     
+    static func randomHash(length: Int? = nil) -> String {
+        let string = "\(UUID().uuidString)\(Date().timeIntervalSince1970)\(UInt64.random(in: 0...UInt64.max))"
+        return string.hash(length: length, default: "").uppercased()
+    }
+    
     func hash(length: Int? = nil) throws -> String {
         try self.utf8data().hash(length: length)
     }
